5e0628a138b41e191cf4275afca89ba9827e9146
saidaraogajula <gajulasaidarao@gmail.com>
2017-06-21 16:30:16 +0530

    first commit

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-var v =5;
-console.log(v);
+const express = require('express')
+const app = express()
+app.get('/',function (req, res) {
+	res.send('hello world!')
+}
+)
+app.listen(3000, function()
+{
+	console.log('Example app listening on port 3000!')
+})
